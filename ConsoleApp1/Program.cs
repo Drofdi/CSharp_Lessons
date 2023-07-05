a5c001e7ced6c986b0072cec0cf990f04c2f286b
drofdi <diavediavy@gmail.com>
2023-07-05 12:01:27 +0600

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program{
+        static void Main(string[] args){
+            //int a = 5, b = 6;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //string data;
+            //data = Console.ReadLine();
+            //Console.WriteLine("Privet " + data);
+
+            //Калькулятор простой на сложение c конвертацией
+            //string str;
+            //int a, b;
+            //Console.WriteLine("Введите первое число");
+            //str = Console.ReadLine();
+            //a = Convert.ToInt32(str);
+            //Console.WriteLine("Введите второе число");
+            //str = Console.ReadLine();
+            //b = Convert.ToInt32(str);
+            //Console.WriteLine(a + b);
+
+            //Напишите программу вычисляющую среднее арифметическое двух чисел
+            int a = 12;
+            int b = 5;
+
+            double result = ((double) a + b) / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
